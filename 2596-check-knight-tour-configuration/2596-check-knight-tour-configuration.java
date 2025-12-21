1a2b610fb5654057e900b28c3f9970e332d2d66e
Madhumita Dutta <162785036+madhumita1301@users.noreply.github.com>
2025-12-21 12:59:17 +0530

    Time: 1 ms (94.42%), Space: 46.7 MB (6.88%) - LeetHub

---
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.java
@@ -0,0 +1,60 @@
+class Solution {
+    public boolean helper(int[][] grid, int row, int col, int num) {
+        int n = grid.length;
+        int i, j;
+        if (grid[row][col] == n * n - 1)
+            return true;
+
+        // 2up 1right
+        i = row - 2;
+        j = col + 1;
+        if (i >= 0 && j < n && grid[i][j] == num + 1)
+            return helper(grid, i, j, num + 1);
+        // 2up 1left
+        i = row - 2;
+        j = col - 1;
+        if (i >= 0 && j >= 0 && grid[i][j] == num + 1)
+            return helper(grid, i, j, num + 1);
+
+        // 2down 1right
+        i = row + 2;
+        j = col + 1;
+        if (i < n && j < n && grid[i][j] == num + 1)
+            return helper(grid, i, j, num + 1);
+        // 2down 1left
+        i = row + 2;
+        j = col - 1;
+        if (i < n && j >= 0 && grid[i][j] == num + 1)
+            return helper(grid, i, j, num + 1);
+
+        // 2right 1up
+        i = row - 1;
+        j = col + 2;
+        if (i >= 0 && j < n && grid[i][j] == num + 1)
+            return helper(grid, i, j, num + 1);
+        // 2right 1 down
+        i = row + 1;
+        j = col + 2;
+        if (i < n && j < n && grid[i][j] == num + 1)
+            return helper(grid, i, j, num + 1);
+
+        // 2left 1up
+        i = row - 1;
+        j = col - 2;
+        if (i >= 0 && j >= 0 && grid[i][j] == num + 1)
+            return helper(grid, i, j, num + 1);
+        // 2left 1 down
+        i = row + 1;
+        j = col - 2;
+        if (i < n && j >= 0 && grid[i][j] == num + 1)
+            return helper(grid, i, j, num + 1);
+        return false;
+
+    }
+
+    public boolean checkValidGrid(int[][] grid) {
+        if (grid[0][0] != 0)
+            return false;
+        return helper(grid, 0, 0, 0);
+    }
+}
