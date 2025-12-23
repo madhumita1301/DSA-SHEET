1cc21cf5c886b85c0b961586b6c457ab5616e795
Madhumita Dutta <162785036+madhumita1301@users.noreply.github.com>
2025-12-23 23:34:21 +0530

    Time: 2 ms (98.68%), Space: 81.8 MB (71.65%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -6,9 +6,24 @@ class Solution {
             cnt[nums[i]]++;
         }
         for (int i = 0; i < cnt.length; i++) {
-            if (cnt[i] >= 2)
+            if (cnt[i] > 1)
                 return i;
         }
         return -1;
     }
 }
+
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         int n = nums.length;
+//         int[] cnt = new int[n + 1];
+
+//         for (int num : nums) {
+//             cnt[num]++;
+//             if (cnt[num] > 1) {
+//                 return num;
+//             }
+//         }
+//         return -1;
+//     }
+// }
