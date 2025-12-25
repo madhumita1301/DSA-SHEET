c2b87f24cc7a6e64b04caabf6341136f996d5d97
Madhumita Dutta <162785036+madhumita1301@users.noreply.github.com>
2025-12-25 17:32:26 +0530

    Time: 0 ms (100%), Space: 43.5 MB (32.5%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,33 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int n = nums.length;
+
+        int l = 0;
+        int mid = 0;
+        int h = n - 1;
+
+        while (mid <= h) {
+
+            if (nums[mid] == 0) {
+                int temp = nums[l];
+                nums[l] = nums[mid];
+                nums[mid] = temp;
+                l++;
+                mid++;
+            }
+
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+
+            else {
+                int temp = nums[h];
+                nums[h] = nums[mid];
+                nums[mid] = temp;
+                h--;
+
+            }
+        }
+
     }
 }
