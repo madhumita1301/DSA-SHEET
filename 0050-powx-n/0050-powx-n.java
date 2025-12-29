3d1ee70113084e7a3b510949f84aa8abbbbb1cd6
Madhumita Dutta <162785036+madhumita1301@users.noreply.github.com>
2025-12-29 18:01:00 +0530

    Time: 0 ms (100%), Space: 48 MB (43%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,20 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
-        long power = n;      // convert to long to avoid overflow
+        // long power = n;      // convert to long to avoid overflow
         double result = 1.0;
+        result = Math.pow(x, n);
 
-        if (power < 0) {
-            x = 1 / x;
-            power = -power;
-        }
+        // if (power < 0) {
+        //     x = 1 / x;
+        //     power = -power;
+        // }
 
-        while (power > 0) {
-            if ((power & 1) == 1) {   // if power is odd
-                result *= x;
-            }
-            x *= x;       // square the base
-            power >>= 1;  // divide power by 2
-        }
+        // while (power > 0) {
+        //     if ((power & 1) == 1) {   // if power is odd
+        //         result *= x;
+        //     }
+        //     x *= x;       // square the base
+        //     power >>= 1;  // divide power by 2
+        // }
 
         return result;
     }
