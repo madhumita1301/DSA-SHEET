8e7821ffe274fcd1f8cce43c9ec9b06b933f1c6c
Madhumita Dutta <162785036+madhumita1301@users.noreply.github.com>
2026-01-02 12:51:36 +0530

    Time: 0 ms (100%), Space: 44.3 MB (14.97%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -3,17 +3,17 @@ class Solution {
         int n = a.length;
         int st = 0;
         int end = n - 1;
-        while (st <= end) {
+        while (st < end) {
             int mid = st + (end - st) / 2;
-            if ((mid == 0 || a[mid] > a[mid - 1]) && (mid == n - 1 || a[mid] > a[mid + 1])) {
-                return mid;
-            }
+            // if ((mid == 0 || a[mid] > a[mid - 1]) && (mid == n - 1 || a[mid] > a[mid + 1])) {
+            //     return mid;
+            // }
             if (a[mid] < a[mid + 1]) {
                 st = mid + 1;
             } else {
-                end = mid - 1;
+                end = mid;
             }
         }
-        return -1;
+        return st;
     }
 }
