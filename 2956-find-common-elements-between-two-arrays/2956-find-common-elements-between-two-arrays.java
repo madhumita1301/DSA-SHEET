b9c6fdf50ea0fa36a3974d26eafd0396c97141b9
Madhumita Dutta <162785036+madhumita1301@users.noreply.github.com>
2026-01-02 13:39:43 +0530

    Time: 6 ms (64.61%), Space: 47.5 MB (29.51%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
@@ -3,27 +3,23 @@ class Solution {
         int ans1 = 0, ans2 = 0;
         int n = nums1.length;
         int m = nums2.length;
-        int freq1[] = new int[101];
-        int freq2[] = new int[101];
 
         HashSet<Integer> set1 = new HashSet<>();
         HashSet<Integer> set2 = new HashSet<>();
         for (int num : nums1) {
-            freq1[num]++;
             set1.add(num);
         }
         for (int num : nums2) {
-            freq2[num]++;
             set2.add(num);
         }
-        for (int x : set1) {
+        for (int x : nums1) {
             if (set2.contains(x)) {
-                ans1 += freq1[x];
+                ans1++;
             }
         }
-        for (int x : set2) {
+        for (int x : nums2) {
             if (set1.contains(x)) {
-                ans2 += freq2[x];
+                ans2++;
             }
         }
         return new int[] { ans1, ans2 };
