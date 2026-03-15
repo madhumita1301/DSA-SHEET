cb42f467a897a6ac96f2ce58a9a908506ef35612
Madhumita Dutta <162785036+madhumita1301@users.noreply.github.com>
2026-03-15 17:36:16 +0530

    Time: 0 ms (100%), Space: 42.7 MB (38.34%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,12 +1,7 @@
 class Solution {
     public int countCommas(int n) {
-       int ans = 0;
-        if (n >= 1000)
-            ans += n - 999;
-        if (n >= 1000000)
-            ans += n - 999999;
-        if (n >= 1000000000)
-            ans += n - 999999999;
-        return ans;
+        if (n < 999)
+            return 0;
+        return (n - 999);
     }
 }
