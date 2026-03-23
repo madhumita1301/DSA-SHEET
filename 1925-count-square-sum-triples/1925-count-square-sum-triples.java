41cd256bb5b7a9e7e97000351723497c114e47ac
Madhumita Dutta <162785036+madhumita1301@users.noreply.github.com>
2026-03-23 19:47:54 +0530

    Time: 4 ms (89.81%), Space: 45.6 MB (7.24%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.java
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -1,31 +1,48 @@
+// // class Solution {
+// //     public int countTriples(int n) {
+// //         int ans = 0;
+// //         for (int i = 1; i <= n; i++) {
+// //             for (int j = i + 1; j <= n; j++) {
+// //                 for (int k = j + 1; k <= n; k++) {
+// //                     if ((i * i + j * j == k * k)) {
+// //                         ans += 2;
+// //                     }
+// //                 }
+// //             }
+// //         }
+// //         return ans;
+// //     }
+// // }
+
 // class Solution {
 //     public int countTriples(int n) {
 //         int ans = 0;
-//         for (int i = 1; i <= n; i++) {
-//             for (int j = i + 1; j <= n; j++) {
-//                 for (int k = j + 1; k <= n; k++) {
-//                     if ((i * i + j * j == k * k)) {
-//                         ans += 2;
-//                     }
+//         for (int a = 1; a <= n; a++) {
+//             for (int b = 1; b <= n; b++) {
+//                 int sum = a * a + b * b;
+//                 int c = (int) Math.sqrt(sum);
+//                 if (c * c == sum && c <= n) {
+//                     ans++;
 //                 }
 //             }
 //         }
 //         return ans;
 //     }
 // }
-
 class Solution {
     public int countTriples(int n) {
-        int ans = 0;
+        boolean[] square = new boolean[2 * n * n + 1];
+        for (int i = 1; i <= n; i++) {
+            square[i * i] = true;
+        }
+        int count = 0;
         for (int a = 1; a <= n; a++) {
             for (int b = 1; b <= n; b++) {
-                int sum = a * a + b * b;
-                int c = (int) Math.sqrt(sum);
-                if (c * c == sum && c <= n) {
-                    ans++;
+                if (square[a * a + b * b]) {
+                    count++;
                 }
             }
         }
-        return ans;
+        return count;
     }
 }
