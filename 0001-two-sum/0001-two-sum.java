5abba26f7af2a6252f42518c3143608abce3e0a4
Madhumita Dutta <162785036+madhumita1301@users.noreply.github.com>
2026-04-04 23:53:27 +0530

    Time: 2 ms (99.29%), Space: 47.1 MB (65.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,28 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // BRUTE FORCE METHOD 
-        // for (int i = 0; i < nums.length; i++) {
-        // for (int j = i + 1; j < nums.length; j++) {
-        // if (nums[i] + nums[j] == target)
-        // return new int[] { i, j };
-        // }
-        // }
-        // return new int[0];
-        // }
-
-        // Using HASHMAP 
         int n = nums.length;
-        int[] ans = { -1 }; // initialized value of the array
-        // makking a HashMap of values of the array and their indices
-        HashMap<Integer, Integer> mp = new HashMap<>();
+        int ans[] = { -1, -1 };
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            int partner = target - nums[i];
-            if (mp.containsKey(partner)) { // if the map contains the other value needed to hit the target
-                ans = new int[] { (mp.get(partner)), i };
+            int required = target - nums[i];
+            if (map.containsKey(required)) {
+                ans = new int[] { (map.get(required)), i };
                 return ans;
-            } else { // if the partner for target is not found then we keep putting the values and
-                     // their indices
-                mp.put(nums[i], i);
+            } else {
+                map.put(nums[i], i);
             }
         }
         return ans;
